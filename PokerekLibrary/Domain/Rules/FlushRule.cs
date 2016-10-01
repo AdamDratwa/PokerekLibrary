a1dcca796124ae49e8d91d49290e912549b72207
adratwa <dratwa.adam@gmail.com>
2016-10-01 21:31:32 +0200

    Flush rule tests

--- PokerekLibrary/Domain/Rules/FlushRule.cs
+++ PokerekLibrary/Domain/Rules/FlushRule.cs
@@ -8,14 +8,7 @@ namespace PokerekLibrary.Domain.Rules
         public bool IsTrue(List<Card> cards)
         {
             cards = cards.OrderBy(x => x.Color == Colors.KARO).ToList();
-            for (int i = 0; i < cards.Count - 1; i++)
-            {
-                if (!(RulePredicates.CardsInColor(cards)))
-                {
-                    return false;
-                }
-            }
-            return true;
+            return RulePredicates.CardsInColor(cards);
         }
 
         public int Power
